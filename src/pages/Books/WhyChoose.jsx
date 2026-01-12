9e86890bb3a9e66628993ff955eaff3ccd3c06c7
Sohel Mamun <GitHub@example.com>
2026-01-12 18:47:03 +0600

    updated

--- src/pages/Books/WhyChoose.jsx
+++ src/pages/Books/WhyChoose.jsx
@@ -27,7 +27,7 @@ const WhyChoose = () => {
         <h2 className="text-3xl md:text-4xl font-bold mb-4 text-gray-900 dark:text-indigo-400">
           Why Choose Book Courier at Home
         </h2>
-        <p className="text-gray-600 dark:text-gray-300 mb-12 max-w-2xl mx-auto">
+        <p className="text-sky-500 dark:text-gray-300 mb-12 max-w-2xl mx-auto">
           Discover why BookCourier is the preferred choice for book lovers everywhere.
         </p>
 
